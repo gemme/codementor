6f876e69a3585ee31456de1ff61bf70765d80553
Gabriel Ernesto Manuel Martinez Escobar <gernestome@hotmail.com>
2018-09-15 15:53:53 -0500

    Remove the now ignored directory node_modules
    enhancing

---
+++ fisrt_challenge/sort_cvs_columns.js
@@ -0,0 +1,22 @@
+exports.sortCsvColumns =  (csv_data) => {
+
+    const rows = csv_data.split('\n');
+    const unorderedNames = rows[0].split(',');
+    const orderedNames = rows[0].split(',').sort((a,b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+
+    const ordered = [orderedNames.join(',')];
+
+      for(let rowIndex = 1 ; rowIndex < rows.length ; rowIndex++){
+
+        const elements = rows[rowIndex].split(',');
+        const row = [];
+
+        for(let colIndex = 0 ; colIndex < elements.length ; colIndex++){
+          const i = unorderedNames.findIndex(v => v === orderedNames[colIndex]);
+          row.push(elements[i]);
+        }
+        ordered.push(row.join(','));
+      }
+      return ordered.join('\n');
+};
