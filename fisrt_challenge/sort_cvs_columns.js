0e0391a92b08af2c733ed0ccd13b766a7342f42e
Gabriel Ernesto Manuel <gernestome@hotmail.com>
2018-09-15 16:39:16 -0500

    Parsing csv file (#2)
    * challenge solved
    
    * adding tests
    
    * adding tests and gitignore
    
    * Remove the now ignored directory node_modules
    
    enhancing

--- fisrt_challenge/sort_cvs_columns.js
+++ fisrt_challenge/sort_cvs_columns.js
@@ -4,11 +4,6 @@ exports.sortCsvColumns =  (csv_data) => {
     const unorderedNames = rows[0].split(',');
     const orderedNames = rows[0].split(',').sort((a,b) => a.toLowerCase().localeCompare(b.toLowerCase()));
 
-    const hashNames = {};
-    orderedNames.forEach((name,index) => {
-      hashNames[index] = name;
-    });
-
     const ordered = [orderedNames.join(',')];
 
       for(let rowIndex = 1 ; rowIndex < rows.length ; rowIndex++){
@@ -17,7 +12,7 @@ exports.sortCsvColumns =  (csv_data) => {
         const row = [];
 
         for(let colIndex = 0 ; colIndex < elements.length ; colIndex++){
-          const i = unorderedNames.findIndex(v => v === hashNames[colIndex]);
+          const i = unorderedNames.findIndex(v => v === orderedNames[colIndex]);
           row.push(elements[i]);
         }
         ordered.push(row.join(','));
